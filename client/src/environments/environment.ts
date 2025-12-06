3be5862da3b7dcdb22f6850c9715512ae61a9f15
SiddharthPatil <siddharthpatilcourses@gmail.com>
2025-12-06 13:33:44 +0530

    public key change in environment file

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production:true,
     apiUrl:'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51SXZgePZ30S0Ksh8gsZ6DThXJJEpLK7nfYGqhys4JESn2M3XLJbwtyZaCewtY2cMjpWBnjTIqVB97sTSnm2OuT4m005AlQAqkE'
+    stripePublicKey: 'pk_test_51SaU4qGiTw6eefzXUssEbPiuxI1gZrUBvTTGx9ErZ2tLWkeynPAMn3QYBhEznQWNmEbKyOnERM0w8i3kHjdW3i9o00OdkiKY25'
 };
